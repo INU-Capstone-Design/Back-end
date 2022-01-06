4825b85cb3db13aa9bddc1935025bbeee5513811
장성태 <wkdtjdxo1234@naver.com>
2022-01-06 13:07:44 +0900

    update app.py, init.sql
    app.py DB 연동 테스트용 코드 작성
    init.sql 테스트용 SQL 작성

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
+import logging
+import pymysql
+import sys
 from flask import Flask, request  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
 
-
 @api.route('/login')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class Login(Resource):
     def post(self):
-        username = request.form['username']
+        # DB 정보
+        host = "localhost"
+        port = 3306
+        database = "mind_db"
+        username = "mungiyo"
+        password = "102489"
+
+        # DB connection 생성
+        try:
+            conn = pymysql.connect(
+                host=host,
+                user=username,
+                passwd=password,
+                database=database,
+                port=port,
+                use_unicode=True,
+                charset='utf8'
+            )
+            cursor = conn.cursor
+        except:
+            logging.error("DB connection failed")
+            sys.exit()
+        
+
+        userid = request.form['username']
         password = request.form['password']
-        if (username == "mungiyo" and password == "1234"):
-            return {"result": "success", "username": username, "password": password}
+
+        query = f"SELECT * FROM Users WHERE userid='{userid}' and password='{password}'"
+        # query = f"SELECT * FROM Users"
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        if (result is not None):
+            return {"result": "success"}
         else:
             return {"result": "fail"}
 
--- init.sql
+++ init.sql
@@ -3,26 +3,27 @@ CREATE DATABASE mind_db;
 USE mind_db;
 
 CREATE TABLE Users(
-    u_pk        INTEGER PRIMARY KEY,
-    userid      VARCHAR(20),
-    password    VARCHAR(30),
+    userid      VARCHAR(20) NOT NULL PRIMARY KEY,
+    password    VARCHAR(30) NOT NULL,
     name        VARCHAR(20),
     email       VARCHAR(40),
-    create_time DATETIME
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Groups(
-    g_pk     INTEGER PRIMARY KEY,
-    u_pk        INTEGER,
+    g_pk        INTEGER AUTO_INCREMENT PRIMARY KEY,
+    userid      VARCHAR(20),
     is_master   TINYINT(1),
-    create_time DATETIME,
-    FOREIGN KEY (u_pk) REFERENCES Users(u_pk)
+    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userid) REFERENCES Users(userid)
 );
 
 CREATE TABLE Workspaces(
-    w_pk        INTEGER PRIMARY KEY,
+    w_pk        INTEGER AUTO_INCREMENT PRIMARY KEY,
     g_pk        INTEGER,
     map_HTML    LONGTEXT,
-    create_time DATETIME,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (g_pk) REFERENCES Groups(g_pk)
 );
+
+INSERT INTO Users(userid, password, name, email) VALUES('test01', '1234', 'jang', 'test@gmail.com');
