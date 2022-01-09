639c8c964ea0966404d2b40399c8828eebf928ee
장성태 <wkdtjdxo1234@naver.com>
2022-01-09 21:10:44 +0900

    Update app.py
    1. overlap checking API 변경
    get -> post

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
 
+# ----회원 정보 관리----
 # Login REST API
-@api.route('/login')  # 데코레이터 이용, '/login' 경로에 클래스 등록
+@api.route('/login')
 class Login(Resource):
     def post(self):
         userid = request.json.get('userid')
@@ -37,10 +38,48 @@ class Login(Resource):
         else:
             return {"result": "fail"}
 
+# Register REST API
+@api.route('/register')
+class Register(Resource):
+    def post(self):
+        username = request.json.get('username')
+        password = request.json.get('password')
+        name = request.json.get('name')
+        email = request.json.get('email')
+
+        # DB connection 생성
+        try:
+            connection = pymysql.connect(
+                user=db["user"],
+                passwd=db["password"],
+                host=db["host"],
+                database=db["database"],
+                charset=db["charset"]
+            )
+            cursor = connection.cursor()
+        except:
+            logging.error("DB connection failed")
+            return {"result": "DB 연결 실패"}
+        
+        query = f"INSERT INTO Users(userid, password, name, email) \
+                  VALUES('{username}', '{password}', '{name}', '{email}')"
+        
+        # query 실행
+        try:
+            cursor.execute(query)
+            # query 성공 시 commit으로 DB에 반영
+            connection.commit()
+            return {'result': 'success'}
+        except:
+            logging.error("INSERT fail")
+            return {"result": "fail"}
+
 # Overlap checking REST API
-@api.route('/login/<string:username>')
+@api.route('/register/overlapcheck')
 class OverlapCheck(Resource):
-    def get(self, username: str):
+    def post(self):
+        username = request.json.get('username')
+
         # DB connection 생성
         try:
             connection = pymysql.connect(
@@ -64,45 +103,28 @@ class OverlapCheck(Resource):
         else:
             return {"result": "No overlap"}
 
-# Register REST API
-@api.route('/register')
-class Register(Resource):
-    def post(self):
-        userid = request.json.get('userid')
-        password = request.json.get('password')
-        name = request.json.get('name')
-        email = request.json.get('email')
-
-        # DB connection 생성
-        try:
-            connection = pymysql.connect(
-                user=db["user"],
-                passwd=db["password"],
-                host=db["host"],
-                database=db["database"],
-                charset=db["charset"]
-            )
-            cursor = connection.cursor()
-        except:
-            logging.error("DB connection failed")
-            return {"result": "DB 연결 실패"}
-        
-        query = f"INSERT INTO Users(userid, password, name, email) \
-                  VALUES('{userid}', '{password}', '{name}', '{email}')"
-        
-        # query 실행
-        try:
-            cursor.execute(query)
-            # query 성공 시 commit으로 DB에 반영
-            connection.commit()
-            return {'result': 'success'}
-        except:
-            logging.error("INSERT fail")
-            return {"result": "fail"}
-
-
+# ----워크스페이스 관리----
+# Workspace create REST API
 @api.route('/workspace/create')
 class WorkspaceCreate(Resource):
+    def post(self):
+        pass
+
+# Workspace read REST API
+@api.route('/workspace/read')
+class WorkspaceRead(Resource):
+    def post(self):
+        pass
+
+# Workspace update REST API
+@api.route('/workspace/update')
+class WorkspaceUpdate(Resource):
+    def post(self):
+        pass
+
+# Workspace delete REST API
+@api.route('/workspace/delete')
+class WorkspaceDelete(Resource):
     def post(self):
         pass
 
