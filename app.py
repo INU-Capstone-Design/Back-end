4668cd3de30cd235fe53d31468ef44af0b307b2a
장성태 <wkdtjdxo1234@naver.com>
2022-01-12 14:06:28 +0900

    Update app.py, Update init.sql
    REST 형식에 맞게 코드 수정
    User CRUD 형식으로 변경
    User C, R 완료 나머지 수정 예정
    
    Users 테이블에 userid 필드 추가
    pk : username -> userid 변경
    username 참조하던 외래키 userid로 변경

--- app.py
+++ app.py
@@ -2,45 +2,15 @@ import logging
 import pymysql
 from config import db
 from flask import Flask, request  # 서버 구현을 위한 Flask 객체 import
-from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
+from flask_restx import Api, Resource, resource  # Api 구현을 위한 Api 객체 import
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
 
 # ----회원 정보 관리----
-# Login REST API
-@api.route('/login')
-class Login(Resource):
-    def post(self):
-        userid = request.json.get('userid')
-        password = request.json.get('password')
-
-        # DB connection 생성
-        try:
-            connection = pymysql.connect(
-                user=db["user"],
-                passwd=db["password"],
-                host=db["host"],
-                database=db["database"],
-                charset=db["charset"]
-            )
-            cursor = connection.cursor()
-        except:
-            logging.error("DB connection failed")
-            return {"result": "DB 연결 실패"}
-        
-        query = f"SELECT * FROM Users WHERE userid='{userid}' and password='{password}'"
-        cursor.execute(query)
-        result = cursor.fetchone()
-
-        if (result is not None):
-            return {"result": "success"}
-        else:
-            return {"result": "fail"}
-
-# Register REST API
-@api.route('/register')
-class Register(Resource):
+# 회원 CREATE
+@api.route('/members')
+class CreateUser(Resource):
     def post(self):
         username = request.json.get('username')
         password = request.json.get('password')
@@ -61,25 +31,32 @@ class Register(Resource):
             logging.error("DB connection failed")
             return {"result": "DB 연결 실패"}
         
-        query = f"INSERT INTO Users(userid, password, name, email) \
-                  VALUES('{username}', '{password}', '{name}', '{email}')"
-        
-        # query 실행
+        # 중복 되는 회원 있는지 확인
         try:
+            query = f"SELECT * FROM Users WHERE username='{username}'"
             cursor.execute(query)
-            # query 성공 시 commit으로 DB에 반영
-            connection.commit()
-            return {'result': 'success'}
+            if cursor.fetchone():
+                return {"result": "user is overlapping"}
         except:
-            logging.error("INSERT fail")
-            return {"result": "fail"}
+            logging.error("Users select query fail")
+            return {"result": "회원 중복 체크 실패"}
 
-# Overlap checking REST API
-@api.route('/register/overlapcheck')
-class OverlapCheck(Resource):
-    def post(self):
-        username = request.json.get('username')
+        # 회원 정보 DB에 추가
+        try:
+            query = f"INSERT INTO Users(userid, password, name, email) \
+                  VALUES('{username}', '{password}', '{name}', '{email}')"
+            cursor.execute(query)
+            connection.commit()
+            return {'result': 'user create success'}
+        except:
+            logging.error("Users insert query fail")
+            return {"result": "회원 DB 추가 실패"}
 
+
+# 특정 회원 정보 READ
+@api.route('/members/<int:userid>')
+class ReadUser(Resource):
+    def get(self, userid):
         # DB connection 생성
         try:
             connection = pymysql.connect(
@@ -92,16 +69,32 @@ class OverlapCheck(Resource):
             cursor = connection.cursor()
         except:
             logging.error("DB connection failed")
-            return {"result": "DB 연결 실패"}
+            return {"result": "DB connect fail"}
         
-        query = f"SELECT * FROM Users WHERE username='{username}'"
-        cursor.execute(query)
+        try:
+            query = f"SELECT * FROM Users WHERE userid={userid}"
+            cursor.execute(query)
+        except:
+            logging.error("Users select query failed")
+            return {"result": "회원 정보 검색 실패"}
+
         result = cursor.fetchone()
+        connection.close()
+        cursor.close()
 
         if (result is not None):
-            return {"result": "overlap"}
+            return {
+                "result": "success",
+                "userid": result[0],
+                "username": result[1],
+                "password": result[2],
+                "name": result[3],
+                "email": result[4],
+                "create_time": str(result[5])
+            }
         else:
-            return {"result": "No overlap"}
+            return {"result": "No User"}
+
 
 # ----워크스페이스 관리----
 # Workspace create REST API
@@ -130,4 +123,4 @@ class WorkspaceDelete(Resource):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
--- init.sql
+++ init.sql
@@ -1,33 +1,34 @@
-DROP DATABASE IF EXISTS test;
-CREATE DATABASE test;
-USE test;
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
+USE testdb;
 
 CREATE TABLE Users (
+    userid      INT NOT NULL AUTO_INCREMENT,
     username    VARCHAR(20) NOT NULL,
     password    VARCHAR(30) NOT NULL,
     name        VARCHAR(20),
     email       VARCHAR(40),
     create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (username)
+    PRIMARY KEY (userId)
 );
 
 CREATE TABLE Workspaces (
-    workspaceId INT NOT NULL AUTO_INCREMENT,
-    username    VARCHAR(20) NOT NULL,
+    workspaceid INT NOT NULL AUTO_INCREMENT,
+    userid      INT,
     map_HTML    LONGTEXT NOT NULL,
     create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (workspaceId),
-    FOREIGN KEY (username) REFERENCES Users(username)
+    FOREIGN KEY (userId) REFERENCES Users(userId)
 );
 
 CREATE TABLE Groupings (
-    username    VARCHAR(20),
+    userid      INT,
     workspaceId INT,
-    PRIMARY KEY (username, workspaceId),
-    FOREIGN KEY (username)      REFERENCES Users(username),
+    PRIMARY KEY (userId, workspaceId),
+    FOREIGN KEY (userId)        REFERENCES Users(userId),
     FOREIGN KEY (workspaceId)   REFERENCES Workspaces(workspaceId)
 );
 
 INSERT INTO Users(username, password, name, email) VALUES('test01', '1234', 'jang', 'test@gmail.com');
-INSERT INTO Workspaces(username, map_HTML) VALUES('test01', 'Hello World');
-INSERT INTO Groupings(username, workspaceId) VALUES('test01', 1)
+INSERT INTO Workspaces(userid, map_HTML) VALUES(1, 'Hello World');
+INSERT INTO Groupings(userid, workspaceid) VALUES(1, 1)
